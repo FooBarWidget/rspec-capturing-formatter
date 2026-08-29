d39316402e63ff54707fa64922ccd863f0303226
Hongli Lai <hongli@hongli.nl>
2026-08-29 11:52:43 +0200

    Bump version to 1.0.1

--- lib/rspec/capturing_formatter/version.rb
+++ lib/rspec/capturing_formatter/version.rb
@@ -4,6 +4,6 @@ module RSpec
   class CapturingFormatter
     # After updating this value, run `bundle exec rake bundle:install -m`
     # to sync all Bundler lockfiles.
-    VERSION = "1.0.0"
+    VERSION = "1.0.1"
   end
 end
